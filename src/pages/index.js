8793d3b79dbcf68dfa2c929f3ea810151200ef02
pavleerg <pavleergovic@gmail.com>
2023-12-28 00:31:49 +0100

    working on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,34 @@
 import Image from "next/image";
-
-const styles = {
-  homePageTitle: {
-    color: '#FFF',
-    fontFamily: "Londrina Solid",
-    fontSize: "6rem",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "normal",
-  },
-  homepageTitleContainer:{
-    WebkitTextStroke: '5px var(--purple)',
-    background: 'url(/bg/bg-homepage.png)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-};
+import "./style.css";
 
 function HomePage() {
   return (
     <div>
-      <div style={styles.homepageTitleContainer}>
+      <div className="homepageTitleContainer">
         <Image src="/icons/logo.png" alt="logo" width={500} height={500} />
         <div>
-          <h1 style={styles.homePageTitle}>FinanceFriends</h1>
-          <div>
-            <button>Register</button>
-            <button>Login</button>
+          <h1 className="homePageTitle">FinanceFriends</h1>
+          <div className="buttonContainer">
+            <button className="registerButton">Register</button>
+            <button className="loginButton">Login</button>
           </div>
+        </div>
+      </div>
+      <div className="introContainer">
+        <div style={{padding: "10px"}}>
+          <p className="text">
+            This revolutionary chatbot is designed to engage and educate
+            children in the nuances of financial literacy. Utilizing
+            sophisticated artificial intelligence, Finance Friends offers
+            interactive, personalized learning experiences, making complex
+            financial concepts accessible and enjoyable for the younger
+            generation. By blending advanced technology with child-friendly
+            content, we&apos;re setting a new standard in empowering the financial
+            leaders of tomorrow.
+          </p>
+        </div>
+        <div>
+          <p>AI</p>
         </div>
       </div>
     </div>
