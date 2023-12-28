9e7e8478c7a0547a82fdc4aee1c55ab46957532a
pavleerg <pavleergovic@gmail.com>
2023-12-28 03:47:42 +0100

    finished homepage

--- src/components/HomePage/FriendsComponent.jsx
+++ src/components/HomePage/FriendsComponent.jsx
@@ -1,15 +1,20 @@
 import "./FriendsComponent.css";
 
 function FriendsComponent({ friend }) {
+  // Determine the order based on friend.side
+  const orderClass = friend.side === 'left' ? 'orderLeft' : 'orderRight';
+
   return (
-    <div className="friendsContainer">
+    <div className={`friendsContainer ${friend.side}`}>
       <div className="friendsWrapper">
         <div className="friendsGrid">
-        <div className="friendsFlex">
-            <div>
-            <img className="" src={friend.image} alt="logo" />
+          {friend.side === 'left' && (
+            <div className="friendsFlex">
+              <div className={`friendsFlex2 ${friend.side}`}>
+                <img className="friendImage" src={friend.image} alt={friend.name} />
+              </div>
             </div>
-        </div>
+          )}
           <div className="friendsFlex">
             <div>
               <div style={{ display: "flex", alignItems: "baseline" }}>
@@ -19,10 +24,17 @@ function FriendsComponent({ friend }) {
               <p className="friendField">{friend.field}</p>
             </div>
             <div>
-              <p className="text friendText ">{friend.firstText}</p>
-              <p className="text friendText ">{friend.secondText}</p>
+              <p className="text friendText">{friend.firstText}</p>
+              <p className="text friendText">{friend.secondText}</p>
             </div>
           </div>
+          {friend.side === 'right' && (
+            <div className="friendsFlex">
+              <div className={`friendsFlex2 ${friend.side}`}>
+                <img className="friendImage" src={friend.image} alt={friend.name} />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
