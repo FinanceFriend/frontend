b9379abbef1b387170bc8b48ef535a36bc9b0f34
pavleerg <pavleergovic@gmail.com>
2024-01-18 23:31:42 +0100

    fixed build errors 2

--- src/components/Profile/ChangeUsernameModal.jsx
+++ src/components/Profile/ChangeUsernameModal.jsx
@@ -1,46 +1,43 @@
 // ChangeUsernameModal.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Popup.css";
-import ChangeUsername from './ChangeUsername';
-import { useAuth } from '@/context/AuthProvider';
+import ChangeUsername from "./ChangeUsername";
+import { useAuth } from "@/context/AuthProvider";
 
 export default function ChangeUsernameModal() {
-    const [modal, setModal] = useState(false);
-    const { currentUser } = useAuth();
-    
-    const toggleModal = () => {
-        setModal(!modal);
-      };
-    
-      useEffect(() => {
-        if(modal) {
-            document.body.classList.add('active-modal')
-        } else {
-            document.body.classList.remove('active-modal')
-        }
-    }, [modal]); 
-    return (
-        <>
-          <button onClick={toggleModal} className="btn-modal">
-            Change username
-          </button>
-          {modal && (
-            <div className="modal">
+  const [modal, setModal] = useState(false);
+  const { currentUser } = useAuth();
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  useEffect(() => {
+    if (modal) {
+      document.body.classList.add("active-modal");
+    } else {
+      document.body.classList.remove("active-modal");
+    }
+  }, [modal]);
+  return (
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        Change username
+      </button>
+      {modal && (
+        <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
             <h2>Change username</h2>
             <p>
-                <ChangeUsername
-                currentUsername={currentUser.username}
-                />
+              <ChangeUsername currentUsername={currentUser.username} />
             </p>
             <button className="close-modal" onClick={toggleModal}>
               CLOSE
             </button>
           </div>
         </div>
-          )}
-          
-        </>
-    )
+      )}
+    </>
+  );
 }
